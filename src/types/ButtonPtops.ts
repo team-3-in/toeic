6113285c260b97679eb3ab80a7d41f20db36bb99
Chooooooo <suntail2002@naver.com>
2024-01-11 21:06:12 +0900

    Feat: 마이그레이션 적용

--- src/types/ButtonPtops.ts
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-export type ButtonProps = {
-  children: ReactNode;
-  textsize?: string;
-  bgcolor?: string;
-  color?: string;
-  id?: string;
-};
--- src/types/SubmitBtnProps.ts
+++
@@ -1,8 +0,0 @@
-import { ReactNode } from "react";
-
-export type SubmitBtnProps = {
-  children: ReactNode;
-  fontSize?: string;
-  bgColor?: string;
-  color?: string;
-};
