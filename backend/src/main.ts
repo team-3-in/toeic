d1055a4e9eaa73e70fb3ef405964ce1c3a942dde
Chooooooo <suntail2002@naver.com>
2024-01-28 02:02:53 +0900

    Docs: API 문서 포맷 변경
    - 패키지 설치
    - Swagger 문서를 Scalar 문서로 변경
    - main.ts에 Scalar 설정 추가
    - /api prefix 제거
    - setGlobalPrefix 제외
    - 문서 url path를 api/swagger -> /reference로 변경
    
    Related to #20

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,7 @@ import { useContainer } from 'class-validator';
 import { swaggerConfig } from './common/config/swagger.config';
 import { pipeOptions } from './common/config/pipe.config';
 import { corsOptions } from './common/config/cors.config';
+import { apiReference } from '@scalar/nestjs-api-reference';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap(): Promise<void> {
@@ -14,9 +15,15 @@ async function bootstrap(): Promise<void> {
     bufferLogs: true,
   });
   const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('/api/swagger', app, document);
 
-  app.setGlobalPrefix('api');
+  app.use(
+    '/reference',
+    apiReference({
+      spec: {
+        content: document,
+      },
+    }),
+  );
   app.use(cookieParser());
   app.enableCors(corsOptions);
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
