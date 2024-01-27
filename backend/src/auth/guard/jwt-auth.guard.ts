94e03d749d7fb73c7f80e75198a0864c22d1747e
Chooooooo <suntail2002@naver.com>
2024-01-28 01:42:55 +0900

    Feat: 인증 custom decorator 추가
    - 컨트롤러에서 리프레시 토큰, 엑세스 토큰을 가져오는 로직을 데코레이터로 분리
    - AllowAny를 데코레이터로 이동
    
    Related to #21

--- backend/src/auth/guard/jwt-auth.guard.ts
+++ backend/src/auth/guard/jwt-auth.guard.ts
@@ -1,7 +1,7 @@
 import { ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
-import { IS_PUBLIC_KEY } from '../auth.constant';
+import { IS_PUBLIC_KEY } from '../decorator/pass-auth.decorator';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
