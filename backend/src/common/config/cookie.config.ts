cb46b6cea2b3b7420b8c55d8d99d1a728da1d7f1
Chooooooo <suntail2002@naver.com>
2024-01-28 02:33:17 +0900

    Feat: 쿠키 만료시간 설정
    - 리프레시 토큰 쿠키의 만료시간을 2주로 설정
    Related to #21

--- backend/src/common/config/cookie.config.ts
+++ backend/src/common/config/cookie.config.ts
@@ -3,4 +3,5 @@ import { CookieOptions } from 'express';
 export const cookieOptions: CookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === 'production',
+  maxAge: 14 * 24 * 60 * 60 * 1000,
 };
