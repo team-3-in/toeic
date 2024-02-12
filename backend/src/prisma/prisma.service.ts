a5f3f72241f5cd58619c2f7217c63d8b9528201c
Chooooooo <suntail2002@naver.com>
2024-02-12 13:13:07 +0900

    Fix: prisma 빌드 에러에 따른 설정 수정
    - schema.prisma 파일에 generator 설정 수정
      - output 경로를 제거
    - prisma.service.ts 파일에 prisma client 설정 수정
    - package.json 파일에 prisma 빌드 명령어 추가

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -3,6 +3,11 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      log: [process.env.NODE_ENV === 'dev' ? 'query' : 'info'],
+    });
+  }
   async onModuleInit() {
     await this.$connect();
   }
