b10e3c07000f805ce48d8de9477965ec4ba1cbc2
Chooooooo <suntail2002@naver.com>
2024-02-16 00:21:11 +0900

    Feat: 토익 문제에서 ResponseEntity 추가
    - CRUD 기능을 위한 API 추가
    - 토익 문제를 추가하고 수정하고 삭제하는 기능을 추가했습니다.
    - Response Entity를 적용하여 클라이언트에게 응답을 보내는 기능을 추가했습니다.
    
    Related to #35

--- packages/backend/src/toeic/toeic.controller.ts
+++ packages/backend/src/toeic/toeic.controller.ts
@@ -1,6 +1,16 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+} from '@nestjs/common';
 import { ToeicService } from './toeic.service';
 import { ApiSwagger } from '../common/swagger/api.decorator';
+import { ResponseEntity } from '../common/entity/response.entity';
+import { PatchToeicWithQuestion } from './dto/patch-quesion.dto';
 
 @Controller('toeic')
 export class ToeicController {
@@ -8,13 +18,44 @@ export class ToeicController {
 
   @Get()
   @ApiSwagger({ name: '토익 문제 조회' })
-  findAll() {
-    return this.toeicService.findAll();
+  async findAll() {
+    const result = await this.toeicService.findAll();
+    return ResponseEntity.OK_WITH(
+      `Successfully find ${result.length} questsions`,
+      result,
+    );
   }
 
   @Get('/:id')
   @ApiSwagger({ name: '토익 문제 조회' })
-  findOne(@Param('id') id: number) {
-    return this.toeicService.findOne(+id);
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    const result = await this.toeicService.findOne(+id);
+    return ResponseEntity.OK_WITH(
+      `Successfully find question id: ${id}.`,
+      result,
+    );
+  }
+
+  @Patch('/:id')
+  @ApiSwagger({ name: '토익 문제 수정' })
+  updateOne(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() request: PatchToeicWithQuestion,
+  ) {
+    const result = this.toeicService.updateOne(+id, request.toEntity());
+    return ResponseEntity.OK_WITH(
+      `Successfully update question id: ${id}.`,
+      result,
+    );
+  }
+
+  @Delete('/:id')
+  @ApiSwagger({ name: '토익 문제 수정' })
+  deleteOne(@Param('id', ParseIntPipe) id: number) {
+    const result = this.toeicService.deleteOne(+id);
+    return ResponseEntity.OK_WITH(
+      `Successfully delete question id: ${id}.`,
+      result,
+    );
   }
 }
